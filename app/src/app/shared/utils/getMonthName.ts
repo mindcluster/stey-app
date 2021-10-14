627a2675f933630387c1bd5b9b9fe30201277883
jjeanjacques10 <jjean.jacques10@gmail.com>
2021-10-13 23:40:55 -0300

    fix months

--- app/src/app/shared/utils/getMonthName.ts
+++ app/src/app/shared/utils/getMonthName.ts
@@ -1,17 +1,17 @@
 export default function getMonthName(item: number): string {
     var month = new Array(12);
     month[0] = "Jan";
-    month[1] = "Feb";
+    month[1] = "Fev";
     month[2] = "Mar";
-    month[3] = "Apr";
-    month[4] = "May";
+    month[3] = "Abr";
+    month[4] = "Mai";
     month[5] = "Jun";
     month[6] = "Jul";
     month[7] = "Aug";
-    month[8] = "Sep";
-    month[9] = "Oct";
+    month[8] = "Set";
+    month[9] = "Out";
     month[10] = "Nov";
-    month[11] = "Dec";
+    month[11] = "Dez";
 
     return month[item];
 }
